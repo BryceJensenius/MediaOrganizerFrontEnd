decfdfe50baa29c81946607a53ce7a5b38bd0cab
Bryce <brycejensenius@gmail.com>
2025-05-30 01:02:51 -0500

    Prints

--- src/components/MediaWatchlist.js
+++ src/components/MediaWatchlist.js
@@ -68,6 +68,7 @@ export default function MediaWatchlist() {
             alert("Movie Media Name cannot be empty");
             return;
         }
+        printf("Adding movie: %s", mediaName);
         setLoading(true);
         fetch("https://api.brycejensenius.xyz/mediaWatch/add", {
             method: "POST",
