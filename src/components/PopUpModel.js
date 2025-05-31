1250c9638a87b7fa65ff99460bb337fe245bf232
Bryce <brycejensenius@gmail.com>
2025-05-31 12:59:24 -0500

    Better styling for detail pages 2

--- src/components/PopUpModel.js
+++ src/components/PopUpModel.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../styles/style.css';
+import '../styles/PopUpModel.css';
 
 const PopUpModel = ({ isVisible, details, onClose }) => {
   if (!isVisible || !Array.isArray(details) || details.length === 0) return null;
