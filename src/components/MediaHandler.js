34075de1ed200f11942891555467bf8cc60f6e8e
Bryce <brycejensenius@gmail.com>
2025-06-06 22:35:51 -0500

    Remove Anna

--- src/components/MediaHandler.js
+++ src/components/MediaHandler.js
@@ -250,7 +250,7 @@ export default function MediaHandler() {
                             fontWeight: '400',
                         }}
                     >
-                        I Loooooooooooooooooooooooooooooooove you
+                        Add Media
                     </Typography>
                     <Box sx={{ mb: 2 }}>
                         <TextField 
