df8c178b60138db66841d7917bc013c2654504be
Bryce Jensenius <146300137+BryceJensenius@users.noreply.github.com>
2026-05-24 10:12:17 -0500

    Update deploy script to auto deploy to pages

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -36,7 +36,7 @@ export default function LoginPage() {
         const authRequest = { username, password };
 
         try {
-            const response = await fetch(`https://tradelens.space${endpoint}`, {
+            const response = await fetch(`https://tradelens.space/media-organizer${endpoint}`, {
                 method: 'POST',
                 headers: { 
                     'Content-Type': 'application/json'
